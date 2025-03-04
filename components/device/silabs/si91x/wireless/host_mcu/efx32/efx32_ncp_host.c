17e2fad2c392d8b1f946d55d1d2c3e1fcacca560
Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
2025-03-05 00:11:03 +0530

    Refactor SPI transfer functions for improved performance and reliability

--- components/device/silabs/si91x/wireless/host_mcu/efx32/efx32_ncp_host.c
+++ components/device/silabs/si91x/wireless/host_mcu/efx32/efx32_ncp_host.c
@@ -1,19 +1,22 @@
 /*******************************************************************************
-* @file  efx32_ncp_host.c
-* @brief 
-*******************************************************************************
-* # License
-* <b>Copyright 2023 Silicon Laboratories Inc. www.silabs.com</b>
-*******************************************************************************
-*
-* The licensor of this software is Silicon Laboratories Inc. Your use of this
-* software is governed by the terms of Silicon Labs Master Software License
-* Agreement (MSLA) available at
-* www.silabs.com/about-us/legal/master-software-license-agreement. This
-* software is distributed to you in Source Code format and is governed by the
-* sections of the MSLA applicable to Source Code.
-*
-******************************************************************************/
+ * @file  efx32_ncp_host.c
+ * @brief
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2023 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#include <platform/silabs/wifi/ncp/spi_multiplex.h>
+#include <platform/silabs/wifi/SiWx/ncp/sl_si91x_ncp_utility.h>
 
 #include "sl_wifi_constants.h"
 #include "sl_si91x_host_interface.h"
@@ -36,6 +39,14 @@
 #include "sl_spidrv_instances.h"
 #include "sl_spidrv_exp_config.h"
 #include "si91x_ncp_spi_config.h"
+
+#if defined(SL_CATLOG_POWER_MANAGER_PRESENT)
+#include "sl_power_manager.h"
+#endif // SL_CATLOG_POWER_MANAGER_PRESENT
+
+#ifdef SL_BOARD_NAME
+#include "sl_board_control.h"
+#endif // SL_BOARD_NAME
 
 // use SPI handle for EXP header (configured in project settings)
 extern SPIDRV_Handle_t sl_spidrv_exp_handle;
@@ -62,13 +73,16 @@ static void spi_dma_callback(struct SPIDRV_HandleData *handle, Ecode_t transferS
   UNUSED_PARAMETER(handle);
   UNUSED_PARAMETER(transferStatus);
   UNUSED_PARAMETER(itemsTransferred);
+#if defined(SL_CATLOG_POWER_MANAGER_PRESENT)
+  sl_power_manager_remove_em_requirement(SL_POWER_MANAGER_EM1);
+#endif
   osSemaphoreRelease(transfer_done_semaphore);
   return;
 }
 
 static void efx32_spi_init(void)
 {
-  SPIDRV_SetBitrate(SPI_HANDLE, 12500000);
+  SPIDRV_SetBitrate(SPI_HANDLE, USART_INITSYNC_BAUDRATE);
 
   // Configure SPI bus pins
   GPIO_PinModeSet(SL_SPIDRV_EXP_RX_PORT, SL_SPIDRV_EXP_RX_PIN, gpioModeInput, 0);
@@ -92,42 +106,6 @@ static void efx32_spi_init(void)
   return;
 }
 
-Ecode_t si91x_SPIDRV_MTransfer(SPIDRV_Handle_t handle,
-                               const void *txBuffer,
-                               void *rxBuffer,
-                               int count,
-                               SPIDRV_Callback_t callback)
-{
-  USART_TypeDef *usart = handle->initData.port;
-  uint8_t *tx          = (txBuffer != NULL) ? (uint8_t *)txBuffer : dummy_buffer;
-  uint8_t *rx          = (rxBuffer != NULL) ? (uint8_t *)rxBuffer : dummy_buffer;
-
-  if (count < 16) {
-    while (count > 0) {
-      while (!(usart->STATUS & USART_STATUS_TXBL)) {
-      }
-      usart->TXDATA = (uint32_t)*tx;
-      while (!(usart->STATUS & USART_STATUS_TXC)) {
-      }
-      *rx = (uint8_t)usart->RXDATA;
-      if (txBuffer != NULL) {
-        tx++;
-      }
-      if (rxBuffer != NULL) {
-        rx++;
-      }
-      count--;
-    }
-    //callback(handle, ECODE_EMDRV_SPIDRV_OK, 0);
-    return ECODE_EMDRV_SPIDRV_OK;
-  } else {
-    SPIDRV_MTransfer(handle, tx, rx, count, callback);
-  }
-
-  return ECODE_EMDRV_SPIDRV_BUSY;
-}
-#endif
-
 void sl_si91x_host_set_sleep_indicator(void)
 {
   GPIO_PinOutSet(SI91X_NCP_SLEEP_CONFIRM_PORT, SI91X_NCP_SLEEP_CONFIRM_PIN);
@@ -145,6 +123,10 @@ uint32_t sl_si91x_host_get_wake_indicator(void)
 
 sl_status_t sl_si91x_host_init(const sl_si91x_host_init_configuration *config)
 {
+#if SL_SPICTRL_MUX
+  sl_si91x_host_spi_multiplex_init();
+#endif // SL_SPICTRL_MUX
+
   init_config.rx_irq      = config->rx_irq;
   init_config.rx_done     = config->rx_done;
   init_config.boot_option = config->boot_option;
@@ -181,22 +163,78 @@ void sl_si91x_host_enable_high_speed_bus()
   return;
 }
 
+sl_status_t sl_si91x_host_spi_cs_assert()
+{
+#if SL_SPICTRL_MUX
+  sl_wfx_host_spi_cs_assert();
+#endif // SL_SPICTRL_MUX
+  return SL_STATUS_OK;
+}
+
+sl_status_t sl_si91x_host_spi_cs_deassert()
+{
+#if SL_SPICTRL_MUX
+  sl_wfx_host_spi_cs_deassert();
+#endif // SL_SPICTRL_MUX
+  return SL_STATUS_OK;
+}
+
 /*==================================================================*/
 /**
- * @fn         sl_status_t sl_si91x_host_spi_transfer(const void *tx_buffer, void *rx_buffer, uint16_t buffer_length)
- * @param[in]  uint8_t *tx_buff, pointer to the buffer with the data to be transferred
- * @param[in]  uint8_t *rx_buff, pointer to the buffer to store the data received
- * @param[in]  uint16_t transfer_length, Number of bytes to send and receive
- * @param[in]  uint8_t mode, To indicate mode 8 BIT/32 BIT mode transfers.
- * @param[out] None
- * @return     0, 0=success
- * @section description
- * This API is used to transfer/receive data to the Wi-Fi module through the SPI interface.
- */
+  * @fn         sl_status_t sl_si91x_host_spi_transfer(const void *tx_buffer, void *rx_buffer, uint16_t buffer_length)
+  * @param[in]  uint8_t *tx_buff, pointer to the buffer with the data to be transferred
+  * @param[in]  uint8_t *rx_buff, pointer to the buffer to store the data received
+  * @param[in]  uint16_t transfer_length, Number of bytes to send and receive
+  * @param[in]  uint8_t mode, To indicate mode 8 BIT/32 BIT mode transfers.
+  * @param[out] None
+  * @return     0, 0=success
+  * @section description
+  * This API is used to transfer/receive data to the Wi-Fi module through the SPI interface.
+  */
 
 sl_status_t sl_si91x_host_spi_transfer(const void *tx_buffer, void *rx_buffer, uint16_t buffer_length)
 {
   osMutexAcquire(ncp_transfer_mutex, osWaitForever);
+
+  uint8_t *tx_buf = (tx_buffer != NULL) ? (uint8_t *)tx_buffer : dummy_buffer;
+  uint8_t *rx_buf = (rx_buffer != NULL) ? (uint8_t *)rx_buffer : dummy_buffer;
+  Ecode_t status  = ECODE_EMDRV_SPIDRV_OK;
+  // If the buffer length is less than the high speed transfer threshold, use blocking SPI transfer
+  if (buffer_length < 16) {
+    while (buffer_length) {
+      *rx_buf = USART_SpiTransfer(SPI_HANDLE->initData.port, *tx_buf);
+      if (tx_buffer != NULL)
+        tx_buf++;
+      if (rx_buffer != NULL)
+        rx_buf++;
+      buffer_length--;
+    }
+    osMutexRelease(ncp_transfer_mutex);
+    return SL_STATUS_OK;
+  }
+  // If the buffer length is greater than the high speed transfer threshold, use DMA transfer
+  status = SPIDRV_MTransfer(SPI_HANDLE, tx_buf, rx_buf, buffer_length, spi_dma_callback);
+
+  if (ECODE_EMDRV_SPIDRV_OK != status) {
+    SILABS_LOG("ERR: SPI failed with error:%x (tx%x rx%x)", status, (uint32_t)tx_buf, (uint32_t)rx_buf);
+    osMutexRelease(ncp_transfer_mutex);
+
+    return SL_STATUS_FAIL;
+  }
+
+  if (osSemaphoreAcquire(transfer_done_semaphore, 1000) != osOK) {
+    int itemsTransferred = 0;
+    int itemsRemaining   = 0;
+    SPIDRV_GetTransferStatus(SPI_HANDLE, &itemsTransferred, &itemsRemaining);
+    SILABS_LOG("ERR: SPI timed out %d/%d (rx%x rx%x)",
+               itemsTransferred,
+               itemsRemaining,
+               (uint32_t)tx_buf,
+               (uint32_t)rx_buf);
+    SPIDRV_AbortTransfer(SPI_HANDLE);
+    osMutexRelease(ncp_transfer_mutex);
+
+    return SL_STATUS_SPI_BUSY;
   }
 
   osMutexRelease(ncp_transfer_mutex);
