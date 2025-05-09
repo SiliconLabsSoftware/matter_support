265a43911ddd88bb09be8e0c249c84219b79e492
Junior Martinez <junior.martinez@silabs.com>
2025-05-09 10:30:20 -0400

    Revert "[Silabs] Updating the autogen and config for SDK 3.5.0 (#110)"
    This reverts commit 8ca67cfcd2ba685cddc26ee38b7b4a458a544fa9.

--- board-support/si91x/siwx917/BRD4338A/autogen/sl_si91x_power_manager_wakeup_handler.h
+++ board-support/si91x/siwx917/BRD4338A/autogen/sl_si91x_power_manager_wakeup_handler.h
@@ -32,7 +32,6 @@
 
 #include "sl_status.h"
 
-#include "sl_si91x_driver_gpio.h"
 
 
 #include "sl_si91x_power_manager_wakeup_source_config.h"
@@ -40,7 +39,6 @@
 
 sl_status_t sl_si91x_power_manager_wakeup_init(void);
 
-sl_status_t sli_si91x_power_manager_gpio_init(void);
 
 
 #endif // SL_SI91X_POWER_MANAGER_WAKEUP_HANDLER_H
