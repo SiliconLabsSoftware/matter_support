700c471700ac605fea41166f1708e3cfc40b51a3
Junior Martinez <67972863+jmartinez-silabs@users.noreply.github.com>
2025-08-20 09:12:06 -0400

    Update support files for sisdk 2025.6.1 and wifisdk 3.5.1 (#151)

--- board-support/si91x/siwx917/BRD2708A/autogen/sl_event_handler.c
+++ board-support/si91x/siwx917/BRD2708A/autogen/sl_event_handler.c
@@ -1,5 +1,6 @@
 #include "sl_event_handler.h"
 
+#include "system_si91x.h"
 #include "rsi_nvic_priorities_config.h"
 #include "sl_si91x_clock_manager.h"
 #include "sli_siwx917_soc.h"
@@ -65,7 +66,8 @@ void sl_service_init(void)
   sli_si91x_power_manager_configure_ram_and_peripheral();
 #endif // SL_ICD_ENABLED
   sl_mbedtls_init();
-  sl_iostream_init_instances();
+  sl_iostream_init_instances_stage_1();
+  sl_iostream_init_instances_stage_2();
 }
 
 void sl_stack_init(void)
@@ -76,9 +78,13 @@ void sl_internal_app_init(void)
 {
 }
 
-void sl_iostream_init_instances(void)
+void sl_iostream_init_instances_stage_1(void)
 {
   sl_iostream_rtt_init();
+}
+
+void sl_iostream_init_instances_stage_2(void)
+{
   sl_iostream_set_console_instance();
 }
 
--- board-support/si91x/siwx917/BRD2911A/autogen/sl_event_handler.c
+++ board-support/si91x/siwx917/BRD2911A/autogen/sl_event_handler.c
@@ -1,5 +1,6 @@
 #include "sl_event_handler.h"
 
+#include "system_si91x.h"
 #include "rsi_nvic_priorities_config.h"
 #include "sl_si91x_clock_manager.h"
 #include "sli_siwx917_soc.h"
@@ -36,6 +37,7 @@ void sli_internal_permanent_allocation(void)
 
 void sl_platform_init(void)
 {
+  SystemCoreClockUpdate();
   sl_si91x_device_init_nvic();
   sl_si91x_clock_manager_init();
   sli_si91x_platform_init();
@@ -65,7 +67,8 @@ void sl_service_init(void)
   sli_si91x_power_manager_configure_ram_and_peripheral();
 #endif // SL_ICD_ENABLED
   sl_mbedtls_init();
-  sl_iostream_init_instances();
+  sl_iostream_init_instances_stage_1();
+  sl_iostream_init_instances_stage_2();
 }
 
 void sl_stack_init(void)
@@ -76,9 +79,13 @@ void sl_internal_app_init(void)
 {
 }
 
-void sl_iostream_init_instances(void)
+void sl_iostream_init_instances_stage_1(void)
 {
   sl_iostream_rtt_init();
+}
+
+void sl_iostream_init_instances_stage_2(void)
+{
   sl_iostream_set_console_instance();
 }
 
